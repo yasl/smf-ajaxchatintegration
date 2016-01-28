001083d7ac6a766e3f43e9cf8a5becb528a5e7c0
yasl <yasl@openmailbox.org>
2016-01-28 11:49:00 +0000

    Add chat_admin permission

--- Shout.english.php
+++ Shout.english.php
@@ -17,6 +17,8 @@ $txt['permissionname_shout_mod'] = 'Moderate shoutBox';
 $txt['permissionhelp_shout_mod'] = 'Give access to moderate the shoutBox.';
 $txt['permissionname_chat_mod'] = 'Moderate chat';
 $txt['permissionhelp_chat_mod'] = 'Give access to moderate the chat.';
+$txt['permissionname_chat_admin'] = 'Administrate chat';
+$txt['permissionhelp_chat_admin'] = 'Give full admin rights over the chat.';
 $txt['permissiongroup_chat'] = 'Chat';
 $txt['chat_settings_desc'] = 'AjaxChat Settings';
 ?>
